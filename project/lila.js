f3d68326a63468c84f54583724e3a98c4f9f0e60
lilakangelaris <125599089+lilakangelaris@users.noreply.github.com>
2025-03-17 18:20:17 +0000

    first

---
+++ project/lila.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(800,800);
+    background(173,216,230);
+  }
+  
+  function draw() {
+    rect(30,420,200,350)
+    rect(300,420,200,350)
+    rect(570,420,200,350)
+    line(266, 0, 266, 800)
+    line(533, 0, 533, 800)
+
+}
